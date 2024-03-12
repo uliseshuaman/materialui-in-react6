61018540d364acf0d6927c3556ba5c8ce2bef0c1
Ulises Huaman <ul.huaman@gmail.com>
2024-03-12 19:46:03 +0100

    yun an form

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BasicCard from '../../components/common/BasicCard/BasicCard';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import SearchBar from '../../components/common/SearchBar/SearchBar';
@@ -8,23 +8,40 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
 import { cardHeaderStyles } from './styles';
+import NewUserModal from '../../components/Modals/NewUserModal/NewUserModal';
 
 const Authentication = () => {
+    const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [searchResults, setSearchResults] = useState(users);
 
     const getHeader = () => {
-        const handleChange = (value) => {
-            console.log(value);
+        const handleSearch = (value) => {
+            filterData(value);
+        };
+
+        const filterData = (value) => {
+            const lowercasedValue = value.toLowerCase().trim();
+            if (lowercasedValue === '') setUsers(searchResults);
+            else {
+                const filteredData = searchResults.filter((item) => {
+                    return Object.keys(item).some((key) => 
+                    item[key].toString().toLowerCase().includes(lowercasedValue)
+                    );
+                });
+                setUsers(filteredData)
+            };
         };
 
         const addUser = () => {
-            console.log('click')
+            setOpen(true);
         };
 
         return (
             <Box sx={cardHeaderStyles.wrapper}>
                 <SearchBar 
                     placeholder="Search by email address, phone number, or user UID"
-                    onChange={(event) => handleChange(event.target.value)}
+                    onChange={(event) => handleSearch(event.target.value)}
                     searchBarWidth='720px'
                 />
                 <Box>
@@ -44,13 +61,30 @@ const Authentication = () => {
         )
     };
 
+    const addNewUser = (data) => {
+        users.push({ ...data });
+        setOpen(false);
+    };
+
     const getContent = () => (
-        <Typography 
-            align="center"
-            sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
-        >
-            No users for this project yet
-        </Typography>
+        <>
+            {
+                users.length ? 
+                    users.map((user) => (
+                        <Box sx={{ marginBottom: '20px' }}>
+                            <Typography>User ID: {user.userId}</Typography>
+                            <Typography>Email: {user.email}</Typography>
+                            <Typography>Phone Number: {user.phoneNumber}</Typography>
+                        </Box>
+                    )) :
+                    <Typography 
+                        align="center"
+                        sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
+                    >
+                        No users for this project yet
+                    </Typography>
+            }
+        </>
     );
 
     return (
@@ -59,6 +93,7 @@ const Authentication = () => {
                 header={getHeader()}
                 content={getContent()}
             />
+            <NewUserModal open={open} onClose={() => setOpen(false)} addNewUser={addNewUser}/>
         </GridWrapper>
     )
 }
